ba6640afb6f7c56831d958e2732411fca1e1d029
wjdaudgy1994@ex-it.co.kr <wjdaudgy1994@ex-it.co.kr>
2025-01-31 17:28:52 +0900

    commit

--- resources/assets/js/place.js
+++ resources/assets/js/place.js
@@ -136,64 +136,67 @@ function fn_place_set(tp, sz) {
 	} else if (tp == 2) {
 		if (sz == 60) {
 			fn_div_container_add();
+			fn_div_container_item_add(8, 0);
+			$("#text-container-left-add--" + 0).val("냉장고");
+			fn_div_container_item_add(8, 0);
+			fn_div_container_item_add(8, 0);
 			$("#text-container-left-add--" + 0).val("스크린");
-			fn_div_container_item_add(5, 0);
-			fn_div_container_item_add(2, 0);
-			fn_div_container_item_add(1, 0);
+			fn_div_container_item_add(8, 0);
 			fn_div_container_add();
-			$("#text-container-left-add--" + 1).val("냉장고");
-			fn_div_container_item_add(8, 1);
-			fn_div_container_item_add(2, 1);
+			fn_div_container_item_add(5, 1);
 			fn_div_container_item_add(1, 1);
-			$("#text-container-left-add--" + 1).val("옷걸이");
-			fn_div_container_item_add(8, 1);
+			fn_div_container_item_add(1, 1);
+			fn_div_container_item_add(1, 1);
+			fn_div_container_item_add(1, 1);
 			fn_div_container_add();
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
 			$("#text-container-left-add--" + 2).val("입구");
-			fn_div_container_item_add(8, 2);
+			$("#text-container-right-add--" + 2).val("옷걸이");
+			fn_div_container_item_add(5, 2);
+			$("#text-container-left-add--" + 2).val("쇼파");
+			fn_div_container_item_add(3, 2);
+			fn_div_container_item_add(3, 2);
 		} else if (sz == 50) {
 			fn_div_container_add();
+			fn_div_container_item_add(8, 0);
+			$("#text-container-left-add--" + 0).val("냉장고");
+			fn_div_container_item_add(8, 0);
+			fn_div_container_item_add(8, 0);
 			$("#text-container-left-add--" + 0).val("스크린");
-			fn_div_container_item_add(5, 0);
-			fn_div_container_item_add(2, 0);
-			fn_div_container_item_add(1, 0);
+			fn_div_container_item_add(8, 0);
 			fn_div_container_add();
-			$("#text-container-left-add--" + 1).val("냉장고");
-			fn_div_container_item_add(8, 1);
-			fn_div_container_item_add(2, 1);
 			fn_div_container_item_add(5, 1);
-			$("#text-container-left-add--" + 1).val("옷걸이");
-			fn_div_container_item_add(8, 1);
+			fn_div_container_item_add(1, 1);
+			fn_div_container_item_add(1, 1);
+			fn_div_container_item_add(1, 1);
+			fn_div_container_item_add(1, 1);
 			fn_div_container_add();
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
 			$("#text-container-left-add--" + 2).val("입구");
-			fn_div_container_item_add(8, 2);
+			$("#text-container-right-add--" + 2).val("옷걸이");
+			fn_div_container_item_add(5, 2);
+			$("#text-container-left-add--" + 2).val("쇼파");
+			fn_div_container_item_add(7, 2);
+			fn_div_container_item_add(3, 2);
 		} else if (sz == 40) {
 			fn_div_container_add();
+			fn_div_container_item_add(8, 0);
+			$("#text-container-left-add--" + 0).val("냉장고");
+			fn_div_container_item_add(8, 0);
+			fn_div_container_item_add(8, 0);
 			$("#text-container-left-add--" + 0).val("스크린");
-			fn_div_container_item_add(5, 0);
-			fn_div_container_item_add(2, 0);
-			fn_div_container_item_add(5, 0);
+			fn_div_container_item_add(8, 0);
 			fn_div_container_add();
-			$("#text-container-left-add--" + 1).val("냉장고");
-			fn_div_container_item_add(8, 1);
-			fn_div_container_item_add(2, 1);
 			fn_div_container_item_add(5, 1);
-			$("#text-container-left-add--" + 1).val("옷걸이");
-			fn_div_container_item_add(8, 1);
+			fn_div_container_item_add(1, 1);
+			fn_div_container_item_add(1, 1);
+			fn_div_container_item_add(1, 1);
+			fn_div_container_item_add(1, 1);
 			fn_div_container_add();
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
-			fn_div_container_item_add(8, 2);
 			$("#text-container-left-add--" + 2).val("입구");
-			fn_div_container_item_add(8, 2);
+			$("#text-container-right-add--" + 2).val("옷걸이");
+			fn_div_container_item_add(5, 2);
+			$("#text-container-left-add--" + 2).val("쇼파");
+			fn_div_container_item_add(7, 2);
+			fn_div_container_item_add(7, 2);
 		}
 	} else if (tp == 3) {
 		if (sz == 90) {
@@ -329,8 +332,8 @@ function fn_place_set(tp, sz) {
 		if (sz == 60) {
 			fn_div_container_add();
 			$("#text-container-left-add--" + 0).val("창고");
-			fn_div_container_item_add(8, 0);
-			fn_div_container_item_add(8, 0);
+			fn_div_container_item_add(5, 0);
+			fn_div_container_item_add(5, 0);
 			$("#text-container-left-add--" + 0).val("화장실");
 			$("#text-container-right-add--" + 0).val("화장실");
 			fn_div_container_item_add(6, 0);
@@ -347,8 +350,8 @@ function fn_place_set(tp, sz) {
 		} else if (sz == 70) {
 			fn_div_container_add();
 			$("#text-container-left-add--" + 0).val("창고");
-			fn_div_container_item_add(8, 0);
-			fn_div_container_item_add(8, 0);
+			fn_div_container_item_add(5, 0);
+			fn_div_container_item_add(5, 0);
 			$("#text-container-left-add--" + 0).val("화장실");
 			$("#text-container-right-add--" + 0).val("화장실");
 			fn_div_container_item_add(4, 0);
@@ -365,8 +368,8 @@ function fn_place_set(tp, sz) {
 		} else if (sz == 80) {
 			fn_div_container_add();
 			$("#text-container-left-add--" + 0).val("창고");
-			fn_div_container_item_add(8, 0);
-			fn_div_container_item_add(8, 0);
+			fn_div_container_item_add(5, 0);
+			fn_div_container_item_add(5, 0);
 			$("#text-container-left-add--" + 0).val("화장실");
 			$("#text-container-right-add--" + 0).val("화장실");
 			fn_div_container_item_add(4, 0);
@@ -384,7 +387,7 @@ function fn_place_set(tp, sz) {
 	} else if (tp == 8) {
 		if (sz == 60) {
 			fn_div_container_add();
-			fn_div_container_item_add(8, 0);
+			fn_div_container_item_add(5, 0);
 			$("#text-container-left-add--" + 0).val("입구");
 			$("#text-container-right-add--" + 0).val("냉장고");
 			fn_div_container_item_add(4, 0);
